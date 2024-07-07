64845bc722013fc3f3197a1b61f2cfa9e73495f3
Jesus <jesus.salazar.v35@gmail.com>
2024-07-07 15:59:24 -0600

    Commit inicial: Agregar frontend y backend

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import app from './app.js'
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+}).on('error', (err) => {
+  console.error(`Error al iniciar el servidor: ${err.message}`);
+});
+
