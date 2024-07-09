5bd23ff69d158e19d04954447c9a498882577b1f
JesusSalazarVidal <jesus.salazar.v35@gmail.com>
2024-07-09 01:39:18 -0600

    Eliminar PruebaYoutube

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import YouTubeSearch from './Components/PrubaYoutube'
-import Login from './Components/Login'
 import LoginPage from './pages/LoginPage'
 import {InnovaProvider} from './Context/InnovaContext'
 import Inicio from './pages/InicioPage'
